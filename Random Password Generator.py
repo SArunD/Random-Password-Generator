f01769b1b328326d19b29ceea5fac259c2a968f6
SArunD <67386158+SArunD@users.noreply.github.com>
2020-08-03 16:16:31 -0400

    Update Random Password Generator.py

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -1,5 +1,6 @@
 import random
 
+# This function creates a 12 character long password containing letters, numbers, and characters
 def passwordGenerator():
     letters = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
     numbers = '1,2,3,4,5,6,7,8,9,10'
@@ -26,7 +27,8 @@ def passwordGenerator():
             placeCount += 1
             continue
     return password
-    
+
+# This asks the user for the number of passwords to generate and prints them
 numPasswords = int(input('How many passwards to generate? '))
 for i in range(1, numPasswords+1):
     pd = passwordGenerator()
