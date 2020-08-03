7223ccf729be56b07a42edd6cef61be87fafd0e8
SArunD <67386158+SArunD@users.noreply.github.com>
2020-08-03 16:14:21 -0400

    Add files via upload

---
+++ Random Password Generator.py
@@ -0,0 +1,33 @@
+import random
+
+def passwordGenerator():
+    letters = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+    numbers = '1,2,3,4,5,6,7,8,9,10'
+    characters = '!,@,#,$,%,&,*'
+    password = ''
+    placeCount = 0
+    for i in range(8):
+        if placeCount < 2:
+            password += random.choice(letters.split(','))
+            password += random.choice(numbers.split(','))
+            password += random.choice(characters.split(','))
+            placeCount += 1
+            continue
+        elif placeCount < 3:
+            password += random.choice(numbers.split(','))
+            password += random.choice(characters.split(','))
+            password += random.choice(letters.split(','))
+            placeCount += 1
+            continue
+        elif placeCount < 4:
+            password += random.choice(characters.split(','))
+            password += random.choice(letters.split(',')).upper()
+            password += random.choice(numbers.split(','))
+            placeCount += 1
+            continue
+    return password
+    
+numPasswords = int(input('How many passwards to generate? '))
+for i in range(1, numPasswords+1):
+    pd = passwordGenerator()
+    print(f'Password #{i}: {pd}')
